0beb7f78e86a7cf9d544067ddc580698f0a48d42
kubersharma001 <kubersharma001@gmail.com>
2021-04-23 11:29:14 +0000

    nabu: Set AAPT config flags
    Change-Id: I157c51e1bf41ba89f8e7f83db54c721eda70083c

--- device.mk
+++ device.mk
@@ -20,6 +20,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Enable Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# AAPT
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_TARGET_VNDK_VERSION := $(PRODUCT_SHIPPING_API_LEVEL)

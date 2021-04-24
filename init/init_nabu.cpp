9e748c13d957c8ad7aa558277b0fd9501dc4f92b
daniml3 <daniel@danielml.dev>
2021-04-25 00:19:31 +0200

    nabu: libinit: recovery improvements
    * Always enable ro.debuggable
    * Enable adb by default
    
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I5d3bf5d8c1bbd3dda29d27a2f1e9fbd3d1d5383a

--- init/init_nabu.cpp
+++ init/init_nabu.cpp
@@ -11,6 +11,9 @@
 #include "vendor_init.h"
 #include "property_service.h"
 
+#include <fs_mgr_dm_linear.h>
+
+
 void property_override(char const prop[], char const value[]) {
     prop_info *pi;
 
@@ -48,4 +51,12 @@ void vendor_load_properties() {
         load_global();
     
     load_more_property();
+
+#ifdef __ANDROID_RECOVERY__
+    std::string buildtype = android::base::GetProperty("ro.build.type", "userdebug");
+    if (buildtype != "user") {
+        property_override("ro.debuggable", "1");
+        property_override("ro.adb.secure.recovery", "0");
+    }
+#endif
 }

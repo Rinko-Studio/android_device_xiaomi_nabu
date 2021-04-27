12e6821e24ede5770bdd3e43049ae4af1bea6343
daniml3 <danimoral1001@gmail.com>
2021-04-27 10:20:20 +0200

    nabu: lights: turn into an AIDL service
    * Based on AOSP's default lights AIDL implementation
    * Remove unused functions
    * Remove unused lights
    * Refactor specific lights handling
    * Stop overriding the stock lights HAL vintf entry
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I92a55e9a0dd9a5dcebc7ec1653dc7aca6126b34d

--- device.mk
+++ device.mk
@@ -87,7 +87,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.nabu
+    android.hardware.lights-service.nabu
 
 # Overlays
 PRODUCT_PACKAGES += \

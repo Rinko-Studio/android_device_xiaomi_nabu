f6d7f8c6ed5a5978933054cc3ae5b1a1d24eac9d
ganeshi4u <ganeshvarmai4u@gmail.com>
2021-11-05 13:27:25 +0000

    nabu: rootdir: Override stock vendor light HAL

--- device.mk
+++ device.mk
@@ -63,7 +63,8 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.recovery.usb.rc \
-    init.recovery.qcom.sh
+    init.recovery.qcom.sh \
+    init.thyme.rc
 
 # fastbootd
 PRODUCT_PACKAGES += \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -23,3 +23,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.qcom.sh
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.nabu.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.nabu.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)

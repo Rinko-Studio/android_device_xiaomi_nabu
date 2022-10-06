67cd2d3592c4b1ada2d05b91a6c6db275f1d720b
kubersharma001 <kubersharma001@gmail.com>
2022-10-06 11:38:44 +0000

    nabu: Move power to system to fix compilation
    device/xiaomi/nabu/power/Android.mk: error: "android.hardware.power-service.nabu (native:product) can not link against android.hardware.power-V1-ndk_platform (native:platform)"
    build/make/core/main.mk:1124: error: exiting from previous errors.
    
    - Also override it

--- power/Android.mk
+++ power/Android.mk
@@ -6,7 +6,6 @@ LOCAL_REQUIRED_MODULES := android.hardware.power@1.2-service.rc
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_STEM := android.hardware.power-service
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libutils android.hardware.power-V1-ndk_platform libbinder_ndk
